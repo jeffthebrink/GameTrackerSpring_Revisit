56a8a7584fa2f06f0dc2045f1c0355744e601b58
jeffthebrink <jeffthebrink@gmail.com>
2017-09-06 12:14:19 -0600

    updated project

---
+++ src/main/java/Brink/gametrackerspringrevisit/GameRepository.java
@@ -0,0 +1,7 @@
+package Brink.gametrackerspringrevisit;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GameRepository extends CrudRepository<Game, Integer>{
+}
--- src/main/java/Brink/gametrackerspringrevisit/GameTrackerController.java
+++ src/main/java/Brink/gametrackerspringrevisit/GameTrackerController.java
@@ -1,4 +1,30 @@
 package Brink.gametrackerspringrevisit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
 public class GameTrackerController {
+
+    @Autowired
+    GameRepository games;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        List<Game> gamesList = (List<Game>) games.findAll();
+        model.addAttribute("games", gamesList);
+        return "home";
+    }
+
+    @RequestMapping(path = "/add-game", method = RequestMethod.POST)
+    public String addGame(String gameName, String gamePlatform, String gameGenre, int gameYear) {
+        Game game = new Game(gameName, gamePlatform, gameGenre, gameYear);
+        games.save(game);
+        return "redirect:/";
+    }
 }
