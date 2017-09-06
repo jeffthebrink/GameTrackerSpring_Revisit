cb24626d1956257bb1bda5c7dd41ac2bba38537f
jeffthebrink <jeffthebrink@gmail.com>
2017-09-06 12:48:44 -0600

    added delete function

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#Game Tracker Spring Revisit
+
+This project is a revisit/update to the previous Game Tracker. It uses the Spring framework, integrating
+JPA and Hibernate to interact with a database. PostgreSQL is used.
--- src/main/java/Brink/gametrackerspringrevisit/GameTrackerController.java
+++ src/main/java/Brink/gametrackerspringrevisit/GameTrackerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -25,6 +26,12 @@ public class GameTrackerController {
     public String addGame(String gameName, String gamePlatform, String gameGenre, int gameYear) {
         Game game = new Game(gameName, gamePlatform, gameGenre, gameYear);
         games.save(game);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/remove-game", method = RequestMethod.POST)
+    public String removeGame(int id) {
+        games.delete(id);
         return "redirect:/";
     }
 }
